6544d77b9f6b59fa7a38052871c3982ddf4e2a0b
kibrian <bkiprotich39@gmail.com>
2020-02-24 20:52:18 +0300

    check 2

---
+++ python-day5/exercise3.py
@@ -0,0 +1,14 @@
+students =dict()
+
+student1=dict()
+student1['regno']='1234'
+student1['name']='Nick'
+
+student2=dict()
+student2['regno']='5678'
+student2['name']='brian'
+
+students['1']=student1
+students['2']=student2
+
+print(students)
