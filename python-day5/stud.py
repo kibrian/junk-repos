24d635a0df8d2873dc072a5283fcb40beda835ee
kibrian <bkiprotich39@gmail.com>
2020-02-24 18:22:46 +0300

    sshh

---
+++ python-day5/stud.py
@@ -0,0 +1,16 @@
+students= dict()
+students['1'] ='peter'
+students['2'] = 'Brian'
+students['3'] = 'nick'
+
+print(students.get("1"))
+students['2'] = 'kip'
+print(students.get('2'))
+
+for student in students:
+    print(students['1'])
+
+for student in students.value()
+    print (nick)
+    
+
